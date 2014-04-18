d11fd715c6d1faafba600608e44a78324ac18fbe
TAwosanya <babatunde.awosanya@yahoo.com>
2014-04-18 19:59:45 +0100

    Completed the makeCacheMatrix and the cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL  # initialize a container or store for the inverse of particular matrix
+     set <- function(y) {   # function to register the matrix in the environment
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x   # function to retrieve a particular matrix stored in environment
+     setinverse <- function(inverse) m <<- inverse  # function to register the inverse of the matrix in the environment
+     getinverse <- function() m  # function to call the inverse of a matrix from environment registry
+     list(set = set, get = get, # output functions
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+     m <- x$getinverse() # calling out inverse in the container or store for a cached matrix
+     if(!is.null(m)) {     # boolean to check if container is not empty and if true, report "message" and output the value stored
+          message("getting cached matrix")
+          return(m)
+     }
+     mymatrix <- x$get()  # if false, retrieve or get the matrix
+     m <- solve(mymatrix, ...)  # solve the matrix
+     x$setinverse(m)  # register the inverse in the store or container in environment
+     m  # output result
 }
